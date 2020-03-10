8877eb29606609bd3f8fd33fd26ee395968f9e9b
Artur Ivanov <ivanov.arthur13@gmail.com>
2020-03-11 02:01:07 +0300

    buttons

--- bot.py
+++ bot.py
@@ -11,59 +11,65 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     )
 
 
-def greet_user(bot, update):
+def greet_user(update, context, user_data):
     text = 'Вызван старт'
     logging.info(text)
     bot_text = 'Привет, я бот помощник.'
     my_keyboard = ReplyKeyboardMarkup([
-                                       ['Как добраться до площадки']
-    #                                  ['Посмотреть расписание докладов'],
-    #                                  ['Проголосовать за доклады'],
-    #                                  ['Посмотреть результаты голосования'],
-    #                                  ['Связаться с организаторами'],
-    #                                  ['Поделиться ботом'],
-    #                                  ['Посмотреть календарь событий']
+                                       ['Как добраться до площадки'],
+                                       ['Посмотреть расписание докладов'],
+                                       ['Проголосовать за доклады'],
+                                       ['Посмотреть результаты голосования'],
+                                       ['Связаться с организаторами'],
+                                       ['Поделиться ботом'],
+                                       ['Посмотреть календарь событий']
                                        ]
                                     )
     update.message.reply_text(bot_text, reply_markup=get_keyboard())
 
-def talk_to_me(bot, update):
-    user_text = (update.message.chat.first_name, update.message.text)
+def talk_to_me(update, context, user_data):
+    user_text = (update.message.chat.first_name, update.message.text, user_data)
     logging.info('User: %s, Chat id: %s, Message: %s', update.message.chat.username,
                  update.message.chat.id, update.message.text)
     update.message.reply_text(user_text, reply_markup=get_keyboard())
 
-def send_location(bot, update):
+def send_location(update, context, user_data):
     send_pic = glob('images/rick.jp*g')
     bot.send_photo(chat_id=update.message.chat.id, photo=open(send_pic, 'rb'), reply_markup=get_keyboard())
 
 def get_keyboard():
     my_keyboard = ReplyKeyboardMarkup([
                                        ['Как добраться до площадки']
-    #                                  ['Посмотреть расписание докладов'],
-    #                                  ['Проголосовать за доклады'],
-    #                                  ['Посмотреть результаты голосования'],
-    #                                  ['Связаться с организаторами']
-    #                                  ['Поделиться ботом'],
-    #                                  ['Посмотреть календарь событий']
+                                       ['Посмотреть расписание докладов'],
+                                       ['Проголосовать за доклады'],
+                                       ['Посмотреть результаты голосования'],
+                                       ['Связаться с организаторами']
+                                       ['Поделиться ботом'],
+                                       ['Посмотреть календарь событий']
                                        ], resize_keyboard=True
                                     )
     return my_keyboard
 
 def main():
-    mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=settings.PROXY)
+    updater = Updater(settings.API_KEY, use_context=True, request_kwargs=settings.PROXY)
     logging.info('Бот запустился.')
-    dp = mybot.dispatcher
+
+    dp = updater.dispatcher
     dp.add_handler(CommandHandler('start', greet_user, pass_user_data=True))
     dp.add_handler(CommandHandler('send', send_location, pass_user_data=True))
-    dp.add_handler(MessageHandler(Filters.regex('^(Как добраться до площадки)$', send_location, pass_user_data=True))
-    dp.add_handler(RegexHandler('^(Посмотреть расписание докладов)$', send_location, pass_user_data=True))
-    #dp.add_handler(RegexHandler('^(Проголосовать за доклады)$', send_location, pass_user_data=True))
-    #dp.add_handler(RegexHandler('^(Посмотреть результаты голосования)$', send_location, pass_user_data=True))
-    #dp.add_handler(RegexHandler('^(Связаться с организаторами)$', send_location, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.regex('^(Как добраться до площадки)$', send_location, pass_user_data=True)))
+    dp.add_handler(MessageHandler(Filters.regex('^(Посмотреть расписание докладов)$', send_location, pass_user_data=True)))
+    dp.add_handler(MessageHandler(Filters.regex('^(Проголосовать за доклады)$', send_location, pass_user_data=True)))
+    dp.add_handler(MessageHandler(Filters.regex('^(Посмотреть результаты голосования)$', send_location, pass_user_data=True)))
+    dp.add_handler(MessageHandler(Filters.regex('^(Связаться с организаторами)$', send_location, pass_user_data=True)))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
 
-    mybot.start_polling()
-    mybot.idle()
+    # log all errors
+    dp.add_error_handler(error)
 
-main()
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
