a214648f633b616562ed228ed3329c6ef89af71f
Artur Ivanov <ivanov.arthur13@gmail.com>
2020-03-21 01:38:00 +0300

    bot

--- bot.py
+++ bot.py
@@ -12,17 +12,10 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO, filename='bot.log'
                     )
 
-
 def greet_user(update, context):
     text = 'Вызван старт'
     logging.info(text)
     bot_text = 'Привет, я бот помощник.'
-    my_keyboard = ReplyKeyboardMarkup([
-                                       ['Добраться до площадки', 'Связаться с организаторами', 'Список докладов'],
-                                       ['Проголосовать за доклады','Посмотреть результаты голосования'],
-                                       ['Поделиться ботом', 'Календарь событий', 'Об Ozon Tech']
-                                       ]
-                                    )
     update.message.reply_text(bot_text, reply_markup=get_keyboard())
 
 def get_keyboard():
@@ -34,16 +27,8 @@ def get_keyboard():
                                     )
     return my_keyboard
 
-
-
 def location(update, context):
     navigation_text = 'Выбери откуда тебе удобно добраться.'
-    my_keyboard_1 = ReplyKeyboardMarkup([
-                                        ['От метро Выставочная', 'От метро Международная'],
-                                        ['От МЦК Деловой центр', 'Я на машине'],
-                                        ['Главное меню']
-                                        ]
-                                    )
     update.message.reply_text(navigation_text, reply_markup=get_keyboard_1())
 
 
@@ -67,18 +52,14 @@ def about(update, context):
     reply_markup = InlineKeyboardMarkup(tech_about)
     update.message.reply_text('Тут можно узнать о технологиях.', reply_markup = reply_markup)
 
-
-
 def send_photo(update, context):
     context.bot.send_photo(chat_id=update.message.chat.id, photo=open('images/rick.jpg', 'rb'), reply_markup=get_keyboard())
-
 
 def talk_to_me(bot, update):
     user_text = (update.message.chat.first_name, update.message.text)
     logging.info('User: %s, Chat id: %s, Message: %s', update.message.chat.username,
                  update.message.chat.id, update.message.text)
     update.message.reply_text(user_text, reply_markup=get_keyboard())
-
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=settings.PROXY)
@@ -105,8 +86,6 @@ def main():
     dp.add_handler(MessageHandler(Filters.regex('^(Поделиться ботом)$'), send_photo, pass_user_data=True))
     dp.add_handler(MessageHandler(Filters.regex('^(Календарь событий)$'), calendar, pass_user_data=True))
     dp.add_handler(MessageHandler(Filters.regex('^(Об Ozon Tech)$'), about, pass_user_data=True))
-
-
     dp.add_handler(MessageHandler(Filters.regex('^(От метро Выставочная)$'), location, pass_user_data=True))
     dp.add_handler(MessageHandler(Filters.regex('^(От метро Международная)$'), location, pass_user_data=True))
     dp.add_handler(MessageHandler(Filters.regex('^(От МЦК Деловой центр)$'), location, pass_user_data=True))
