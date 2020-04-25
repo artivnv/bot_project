300508a3595d10c6a1d992b61f958a993e3d7d46
Artur Ivanov <ivanov.arthur13@gmail.com>
2020-04-26 02:29:04 +0300

    class Upload to db

--- webapp/views.py
+++ webapp/views.py
@@ -1,9 +1,13 @@
-from flask import flash, redirect, url_for
+from flask import flash, redirect, url_for, request
 from flask_admin import BaseView, expose
 from flask_admin.contrib.mongoengine import ModelView
+from flask.views import MethodView
+from wtforms import form
 
 import flask_admin as admin
 import flask_login as login
+
+from forms import EventForm
 
 # Create customized model view class
 class MyModelView(ModelView):
@@ -18,6 +22,19 @@ class MyAdminView(admin.BaseView):
     @admin.expose('/')
     def index(self):
         return self.render('bot_ed.html')
+
+    @admin.expose_plugview('/upload/')
+    class Upload(MethodView):
+
+        def post(self, cls):
+            title = 'Отправка данных в базу'
+            form = EventForm(request.form)
+            print(request.form)
+
+            #form.file.data.save()
+            flash('Данные о мероприятии записались в базу')
+            return cls.render('index.html', request=request, name="upload")
+
 
 class MyAdminVote(admin.BaseView):
     @admin.expose('/')
